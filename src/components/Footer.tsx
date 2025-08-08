7c98139fa015b61bc222cb28cd47bb22053bcfac
korbyA <100809495+korbyA@users.noreply.github.com>
2025-08-08 16:22:21 +1000

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -55,7 +55,7 @@ const Footer = () => {
                 title="Join our Discord"
                 onClick={() => window.open('https://discord.gg/67zpNPHED6', '_blank')}
               >
-                <HeartPulse className="w-7 h-7" aria-hidden="true" />
+                <HeartPlus className="w-7 h-7" aria-hidden="true" />
               </Button>
             </div>
           </div>
